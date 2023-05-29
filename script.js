2dbc487a0697899d9a8cc511321c7193e54c3101
Mariam1188 <mashulik886@gmail.com>
2023-05-29 10:27:33 +0400

    addes new exercise

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 "use strict";
 
-const title = document.querySelector("#title");
-title.innerHTML = "DOM";
 
-const text = document.querySelector(".text");
-text.addEventListener("keyup", function(event) {
-    title.innerHTML = event.target.value;
+const btn = document.querySelector("button");
+
+btn.addEventListener("mouseenter", function(event) {
+    event.target.style = `
+    background: green;
+    cursor: pointer;`
 });
 
-text.addEventListener("mouseenter", function() {
-    text.style.border = "2px solid green";
+btn.addEventListener("mouseleave", function(event) {
+    event.target.style.background = "limegreen";
+    
 });
 
-text.addEventListener("mouseleave", function() {
-    text.style.border = "2px solid #3cb371";
-});
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
 
-
-
-const circle = document.querySelector("div");
-
-circle.addEventListener("mouseenter", function() {
-    circle.style.background = "#fce837";
-});
-
-circle.addEventListener("mouseleave", function() {
-    circle.style.background = "#3cb371";
+btn.addEventListener("click", () => {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.body.style.backgroundColor = rndCol;
 });
 
 
